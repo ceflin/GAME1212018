f5a35ada002f984f1198056a3a823046c18b1dac
ceflin <celfin@stumail.jccc.edu>
2018-02-19 14:17:55 -0600

    New ProjectOne for grading

---
+++ Projects/ProjectOne/Assets/Scripts/OnDestroy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnDestroy : MonoBehaviour {
+
+    //public GameObject bullet;
+    //public GameObject enemy;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Ammo")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
